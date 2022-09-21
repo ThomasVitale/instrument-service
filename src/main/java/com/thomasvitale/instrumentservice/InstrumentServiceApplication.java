650ab8ec6ac12e0944e235101a19fd3ac4ce0eb7
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2022-09-21 12:15:11 +0200

    Implement endpoint

--- src/main/java/com/thomasvitale/instrumentservice/InstrumentServiceApplication.java
+++ src/main/java/com/thomasvitale/instrumentservice/InstrumentServiceApplication.java
@@ -1,13 +1,28 @@
 package com.thomasvitale.instrumentservice;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.RouterFunctions;
+import org.springframework.web.servlet.function.ServerResponse;
 
 @SpringBootApplication
 public class InstrumentServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InstrumentServiceApplication.class, args);
+	}
+
+	@Bean
+	RouterFunction<ServerResponse> routerFunction() {
+		return RouterFunctions.route()
+			.GET("/", request -> ServerResponse.ok().body(List.of(
+				"piano", "guitar", "drums", "violin"
+			)))
+			.build();
 	}
 
 }
